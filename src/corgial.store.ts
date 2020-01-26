1da07c6dfa08045bbd6f333df6af1bacaa29eb3d
Almas Barzhaxynov <almasb@synergybis.com>
2020-01-26 11:58:35 -0500

    added songs to save in local db and render in library

--- src/corgial.store.ts
+++ src/corgial.store.ts
@@ -2,6 +2,10 @@ import RxDB, { RxDatabase, RxCollection } from "rxdb";
 import PouchAdapterMemory from "pouchdb-adapter-memory";
 import PouchAdapterHttp from "pouchdb-adapter-http";
 import { Subject, Subscription, from, BehaviorSubject } from "rxjs";
+import * as mm from "music-metadata-browser";
+import { v4 } from "uuid";
+import { IPicture } from "music-metadata/lib/type";
+
 import { SongProps, songSchema } from "./rxdb/schemas/song.schema";
 import { PlaylistProps, playlistSchema } from "./rxdb/schemas/playlist.schema";
 
@@ -54,7 +58,6 @@ export default class CorgialStore {
           this.status.next("ready");
           break;
         }
-
         default: break;
       }
     });
@@ -64,5 +67,44 @@ export default class CorgialStore {
     await this.db.collection({ name: "songs", schema: songSchema });
     await this.db.collection({ name: "playlists", schema: playlistSchema });
     this.events.next({ type: "DB_COLLECTIONS_CREATED", payload: true });
+  }
+
+  async saveDetails(file: File) {
+    const metadata = await mm.parseBlob(file);
+    const id = v4();
+    let title = metadata.common.title;
+    const { artists, genre, album, picture } = metadata.common;
+    let pictureUrl: string = "";
+    if (!title) {
+      title = file.name;
+    }
+    if (picture && picture.length) {
+      pictureUrl = await this.savePicture(id, picture[0]);
+    }
+
+    const payload: SongProps = {
+      id,
+      title,
+      genre,
+      album,
+      artists,
+      tags: ["new"],
+      favorite: false,
+      skipped: 0,
+      played: 0,
+      filename: file.name,
+      picture: pictureUrl,
+      playlists: []
+    };
+    await this.db.songs.atomicUpsert(payload);
+    console.log("added to db");
+  }
+
+  async savePicture(id: string, picture: IPicture) {
+    const name = `${id}.${picture.format.split("/")[1]}`;
+    const blob = new Blob([picture.data], { type: picture.format });
+    const url = URL.createObjectURL(blob);
+    console.log(name, url);
+    return name;
   }
 }
--- src/router/Library.tsx
+++ src/router/Library.tsx
@@ -1,10 +1,32 @@
-import React from "react";
-import { Pane, Text } from "evergreen-ui";
+import React, { useState, useEffect, useContext } from "react";
+import { Pane } from "evergreen-ui";
+import { SongProps } from "../rxdb/schemas/song.schema";
+import { RxDocument } from "rxdb";
+import CorgialContext from "../Corgial.Context";
 
 export const Library: React.FC<{}> = () => {
+  const context = useContext(CorgialContext);
+  const [songs, setSongs] = useState<RxDocument<SongProps>[]>([]);
+
+  useEffect(() => {
+    const fetchSongs = async () => {
+      const music = await context.db.songs.find().exec();
+      setSongs(music);
+    };
+    fetchSongs();
+  }, [context.db.songs]);
+
   return (
     <Pane>
-      <Text>Library</Text>
+      {
+        songs.map((song) => {
+          return (
+            <Pane key={song.id}>
+              {song.title}
+            </Pane>
+          );
+        })
+      }
     </Pane>
   );
 };
